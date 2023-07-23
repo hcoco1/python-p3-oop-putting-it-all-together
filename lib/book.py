9628fb13d6881d5687eee3c7744748555859f512
Ivan <arias.ivan@gmail.com>
2023-07-23 06:15:49 -0400

    lab done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+            
+#The Book class defines a book object. The book object has two attributes: title and page_count. The title attribute is a string that represents the title of the book. The page_count attribute is an integer that represents the number of pages in the book.
+
+# The __init__() method initializes the Book object. The __init__() method takes two arguments: title and page_count. The title argument is the title of the book. The page_count argument is the number of pages in the book.
+
+# The get_page_count() method returns the value of the page_count attribute. The set_page_count() method sets the value of the page_count attribute. The set_page_count() method validates the value of the page_count argument, ensuring that it is an integer.
+
+# The page_count attribute is a property. A property is a special type of attribute that can be used to get or set the value of an attribute. The page_count property is defined using the property() decorator. The property() decorator takes two arguments: the get_page_count() method and the set_page_count() method.
+
+# The turn_page() method simply prints a message that says "Flipping the page...wow, you read fast!".      
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+   
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+        
+# The Shoe class defines a shoe object. The shoe object has two attributes: brand and size. The brand attribute is a string that represents the brand of the shoe. The size attribute is an integer that represents the size of the shoe.
+
+#The __init__() method initializes the Shoe object. The __init__() method takes two arguments: brand and size. The brand argument is the brand of the shoe. The size argument is the size of the shoe.
+
+#The get_size() method returns the value of the size attribute. The set_size() method sets the value of the size attribute. The set_size() method validates the value of the size argument, ensuring that it is an integer.
+
+#The size property is a special type of attribute that can be used to get or set the value of an attribute. The size property is defined using the property() decorator. The property() decorator takes two arguments: the get_size() method and the set_size() method.
+
+#The cobble() method simply prints a message that says "Your shoe is as good as new!".
+
+#The property() decorator is a way to create a property that has the same behavior as the get_size() and set_size() methods. The property() decorator takes two arguments: the get_size() method and the set_size() method. The property() decorator creates a property that can be used to get or set the value of the size attribute.
